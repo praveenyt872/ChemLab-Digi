09aae96ea3585686d9633834cfab249db34c9af7
Praveen <praveenyt872@gmail.com>
2026-08-09 23:16:38 +0530

    fix_pwa_workbox_cache_busting_and_no_cache_meta_tags

--- vite.config.js
+++ vite.config.js
@@ -40,6 +40,8 @@ export default defineConfig({
         ]
       },
       workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
         maximumFileSizeToCacheInBytes: 15 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json,woff,woff2}'],
         runtimeCaching: [
